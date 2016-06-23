32330f8b1029d400d6b2810b5201be0314615c89
Matias Insaurralde <matias@insaurral.de>
2016-06-23 03:02:51 -0400

    Locating __TEXT

--- binarypatch.go
+++ binarypatch.go
@@ -1,1 +1,45 @@
 package binarypatch
+
+import(
+  "io/ioutil"
+  "bytes"
+)
+
+type Binarypatch struct {
+  Filename string
+  Filedata []byte
+  Arch string
+}
+
+func New( filename string, arch string ) *Binarypatch {
+  var err error
+  b := Binarypatch{Filename: filename, Arch: arch}
+  b.Filedata, err = ioutil.ReadFile( filename)
+
+  if err != nil {
+    panic(err)
+  }
+
+  return &b
+}
+
+func( b *Binarypatch ) Locate() ( index int) {
+  switch b.Arch {
+  case "darwin":
+    str := []byte("__TEXT")
+    for i, v := range b.Filedata {
+      n := 0
+      byteGroup := []byte{v}
+      for n <= len(str) && i < len(b.Filedata) - len(str) {
+        byteGroup = append(byteGroup, b.Filedata[i+n])
+        n++
+      }
+
+      if bytes.Index( byteGroup, str ) == 0 {
+        index = i + len(str)
+        break
+      }
+    }
+  }
+  return index
+}
