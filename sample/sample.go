286f64209ab8798e59dc95a658b0766c6684f2ad
Matias Insaurralde <matias@insaurral.de>
2016-06-23 03:26:05 -0400

    Sample code

--- sample/sample.go
+++ sample/sample.go
@@ -2,8 +2,13 @@ package main
 
 import(
   "fmt"
+
+  "github.com/matiasinsaurralde/binarypatch"
 )
 
 func main() {
   fmt.Println("Hello")
+
+  myself := binarypatch.ReadMyself(5, -1, "darwin")
+  fmt.Println("Reading from myself: ", myself, string(myself) )
 }
