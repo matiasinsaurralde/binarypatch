72e45d29b0f4fb9c4685cd70bae2323fb822514d
Matias Insaurralde <matias@insaurral.de>
2016-06-24 00:23:29 -0400

    Adjusting samples build script

--- sample/build_samples.sh
+++ sample/build_samples.sh
@@ -3,6 +3,6 @@ echo "Building sample programs"
 go build -o sample_darwin64 sample.go
 GOOS=linux GOARCH=386 go build -o sample_linux32 sample.go
 GOOS=linux GOARCH=amd64 go build -o sample_linux64 sample.go
-GOOS=windows GOARCH=386 go build -o sample_win32.exe sample.go
-GOOS=windows GOARCH=amd64 go build -o sample_win64.exe sample.go
+GOOS=windows GOARCH=386 go build -o sample_win32.exe sample_windows.go
+GOOS=windows GOARCH=amd64 go build -o sample_win64.exe sample_windows.go
 echo "Done."
